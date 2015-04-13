9a85dda64d22b174ba19ab92ca5aaebe0e61b2b5
Nils Nordal <mb1nonil@hotmail.com>
2015-04-13 15:28:01 +0200

    bigpush

---
+++ KalmarBSK/KalmarBSK.DataAccess/Partials/Meeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalmarBSK.DataAccess
+{
+    public partial class Meeting
+    {
+        public bool IsUpcoming { get { return this.Date > DateTime.Now; } }
+    }
+}
--- KalmarBSK/KalmarBSK.Mvc/Controllers/HomeController.cs
+++ KalmarBSK/KalmarBSK.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KalmarBSK.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +13,41 @@ namespace KalmarBSK.Mvc.Controllers
         {
             return View();
         }
+
+        public ActionResult Meetings()
+        {
+            List<Meeting> list;
+            using (var ctx = new KalmarBSKEntities())
+            {
+                list = ctx.Meetings.Take(20).ToList();
+            }
+            return View(list);
+        }
+
+
+        public ActionResult Members()
+        {
+            return View();
+        }
+
+
+        public ActionResult SearchFilter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchFilter(string search)
+        {
+            List<Meeting> list;
+            using (var ctx = new KalmarBSKEntities())
+            {
+                list = ctx.Meetings.Where(x => x.Location.Contains(search)).ToList();
+            }
+            return View(list);
+        }
+        
+
+
     }
 }
